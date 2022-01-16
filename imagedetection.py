fc32486e84fcbf6bfd42003a0c598d328c5747d7
Serly Setyani <sherly.setyani@gmail.com>
2022-01-16 17:58:14 +0700

    Add Output

--- imagedetection.py
+++ imagedetection.py
@@ -1,4 +1,4 @@
 from lexicograph import detect
 
-detect.detect('D:/College/Semester 5/Forensik/Tubes Forensik/asset/school bus duplikat.png',
+detect.detect('D:/College/Semester 5/Forensik/Tubes Forensik/asset/Tiger_remove.png',
               'D:/College/Semester 5/Forensik/Tubes Forensik/output', block_size=32)
